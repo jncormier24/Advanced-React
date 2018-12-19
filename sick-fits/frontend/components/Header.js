5dd8fc34af660438feabe0a7316ea48b2bff1700
Joe <jncormier24@gmail.com>
2018-12-19 13:44:37 -0500

    - initial commit - added some components and base pages

---
+++ sick-fits/frontend/components/Header.js
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import Nav from "./Nav";
+
+const Header = () => (
+  <div>
+    <div className="bar">
+      <Link href="/">
+        <a>Sick Fits</a>
+      </Link>
+      <Nav />
+    </div>
+    <div className="sub-bar">
+      <p>Search</p>
+    </div>
+    <div className="cart">
+      <p>Cart</p>
+    </div>
+  </div>
+);
+
+export default Header;
