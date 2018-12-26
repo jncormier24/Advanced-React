dc4f93e665f869a44a72ca8e435da3b2597bc8bd
Joe <jncormier24@gmail.com>
2018-12-26 13:58:51 -0500

    Updated pages and components

---
+++ sick-fits/frontend/pages/items.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Items extends Component {
+  render() {
+    return <p>Items!</p>;
+  }
+}
---
+++ sick-fits/frontend/pages/me.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Me extends Component {
+  render() {
+    return <p>Account!</p>;
+  }
+}
